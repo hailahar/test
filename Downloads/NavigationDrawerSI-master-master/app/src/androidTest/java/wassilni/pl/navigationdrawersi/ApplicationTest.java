667453251a12d3402a1bc8b46a9506d8b5642526
hailah <433201450@student.ksu.edu.sa>
2016-11-13 13:21:31 +0300

    commit test

---
+++ Downloads/NavigationDrawerSI-master-master/app/src/androidTest/java/wassilni/pl/navigationdrawersi/ApplicationTest.java
@@ -0,0 +1,14 @@
+package wassilni.pl.navigationdrawersi;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+/////haialh test
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
